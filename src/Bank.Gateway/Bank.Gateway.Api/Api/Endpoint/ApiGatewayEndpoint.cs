2825d2183c6071ba78f30c3049ad889eaa33bd9f
Attila Abreu <attila.abreu@leste.com>
2026-06-29 17:11:06 -0300

    first commit

---
+++ src/Bank.Gateway/Bank.Gateway.Api/Api/Endpoint/ApiGatewayEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bank.Gateway.Api.Application.Models;
+
+namespace Bank.Gateway.Api.Api.Endpoint
+{
+    public class ApiGatewayEndpoint
+    {
+        public static void GatewayEndpoint(WebApplication app)
+        {
+            app.MapPost("/api-gateway", ([FromBody] EndPointModel modelRequest) =>
+            {
+                return modelRequest;
+            });
+        }
+    }
+}
---
+++ src/Bank.Gateway/Bank.Gateway.Api/Program.cs
@@ -0,0 +1,7 @@
+using Bank.Gateway.Api.Api.Endpoint;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+ApiGatewayEndpoint.GatewayEndpoint(app);
+app.Run();
