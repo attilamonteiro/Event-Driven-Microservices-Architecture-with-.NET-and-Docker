0c399403dbabd4f01d38d2b6a437c76d1d75395e
Attila Abreu <attila.abreu@leste.com>
2026-06-30 18:00:37 -0300

    Add Bank.Notification.Api project with Cosmos DB integration
    Added Bank.Notification.Api to the solution with initial structure, including NotificationEntity, IDatabaseService, and DatabaseService using Cosmos DB. Included minimal API setup, HTTP test file, launch settings, and configuration files for logging and hosting.

---
+++ src/Bank.Notification/Bank.Notification.Api/Application/Database/IDatabaseService.cs
@@ -0,0 +1,10 @@
+﻿using Bank.Notification.Api.Domain.Entities.Notification;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bank.Notification.Api.Application.Database
+{
+    public interface IDatabaseService
+    {
+        public DbSet<NotificationEntity> Notifications { get; set; }
+    }
+}
---
+++ src/Bank.Notification/Bank.Notification.Api/Persistence/Database/DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿using Bank.Notification.Api.Domain.Entities.Notification;
+using Bank.Notification.Api.Application.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Azure.Cosmos;
+
+namespace Bank.Notification.Api.Persistence.Database
+{
+    public class DatabaseService: IDatabaseService
+    {
+        private readonly CosmosClient cosmosClient;
+        private readonly Container container;
+        public DbSet<NotificationEntity> Notifications {  get; set; }
+        public DatabaseService(IConfiguration configuration)
+        { 
+            string connectionString = configuration["NOTIFICATIONDBCONTR"];
+            string dataBase = configuration["NOTIFICATIONDBNAME"];
+            string containerName = configuration["NOTIFICATIONDBCONTAINER"];
+            cosmosClient = new CosmosClient(connectionString);
+            container = cosmosClient.GetContainer(dataBase, containerName);
+        }
+
+    }
+}
