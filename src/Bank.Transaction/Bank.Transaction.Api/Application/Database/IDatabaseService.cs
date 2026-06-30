735b2af6148c4698e6122c7eed0019e31d3bc0e3
Attila Abreu <attila.abreu@leste.com>
2026-06-30 08:23:06 -0300

    Add EF Core, DI, and SQL Server integration for transactions
    - Added Entity Framework Core and SQL Server NuGet packages
    - Introduced IDatabaseService interface and implemented in DatabaseService
    - Exposed Transaction DbSet and SaveAsync method
    - Configured DI for DatabaseService and SQL Server connection
    - Updated GET endpoint to fetch transactions from database

---
+++ src/Bank.Transaction/Bank.Transaction.Api/Application/Database/IDatabaseService.cs
@@ -0,0 +1,11 @@
+﻿using Bank.Transaction.Api.Domain.Entities.Transaction;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bank.Transaction.Api.Application.Database
+{
+    public interface IDatabaseService
+    {
+        public DbSet<TransactionEntity> Transaction { get; set; }
+        public Task<bool> SaveAsync();
+    }
+}
--- src/Bank.Transaction/Bank.Transaction.Api/Program.cs
+++ src/Bank.Transaction/Bank.Transaction.Api/Program.cs
@@ -1,11 +1,23 @@
+using Bank.Transaction.Api.Application.Database;
+using Bank.Transaction.Api.Persistence.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DatabaseService>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TRANSACTIONSQLDBCONSTR"));
+});
+
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
 
 var app = builder.Build();
 
-
-app.MapGet("/transction", () =>
+app.MapGet("/transction", async ([FromServices] IDatabaseService _databaseService) =>
 {
+    var data = await _databaseService.Transaction.ToListAsync();
+    return data;
 
 });
 
