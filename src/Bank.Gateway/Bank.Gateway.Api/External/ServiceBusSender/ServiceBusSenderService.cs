af279a820c58aa5b6b3117dab7b7553207fed323
Attila Abreu <attila.abreu@leste.com>
2026-07-01 18:00:39 -0300

    Add ServiceBus sender service and Azure SDK package
    Introduced ServiceBusSenderService for sending messages to Azure Service Bus topics (message sending logic pending). Added Azure.Messaging.ServiceBus package reference and cleaned up project folder entries.

---
+++ src/Bank.Gateway/Bank.Gateway.Api/External/ServiceBusSender/ServiceBusSenderService.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+
+namespace Bank.Gateway.Api.External.ServiceBusSender
+{
+    public class ServiceBusSenderService
+    {
+        private readonly ServiceBusClient _client;
+        private readonly string _topicName;
+        public ServiceBusSenderService(IConfiguration _configuration)
+        {
+            _client = new ServiceBusClient(_configuration["SERVICEBUSCONSTR"]);
+            _topicName = _configuration["SERVICEBUSTOPIC"];
+        }
+        public async Task Execute(object eventModel, string subscription)
+        {
+            await using var send = _client.CreateSender(_topicName);
+            string messageBody = JsonSerializer.Serialize(eventModel);
+
+            ServiceBusMessage message = new ServiceBusMessage(messageBody)
+            {
+              
+            };
+        }
+    }
+}
